66b3310db631f3a52c897dec768328426b24aac4
wangliang <wangliangpeking@gmail.com>
2015-11-14 13:30:22 +0800

    Initial working version

--- README.md
+++ README.md
@@ -1,1 +1,207 @@
 # sina-weibo-crawler
+
+# 抽取数据段
+
+| 段域 | 数据类型 | 含义 | 附加说明 |
+|-------------|-------------|------------|---------------|
+| url | string | 主页的链接地址 | |
+| nickname | string | 昵称 | |
+| verify_type | string | 认证类型 | |
+| verify_info | string | 认证信息 | |
+| num_fans | int | 粉丝数目 | |
+| num_follow | int | 关注数目 | |
+| num_weibo | int | 发布微博数目 | |
+| gender | string  | 性别 | |
+| birthday | string | 生日 | 部分情况下新浪返回星座 |
+| weibo | array | 用户所发微博 | 数组每一个元素为微博文本 |
+| fans | array | 用户粉丝信息 | 每一个元素包含num_fans, url, nickname, verify_info |
+| follow | array | 用户关注信息 | 每一个元素包含num_fans, url, nickname, verify_info |
+| location | string | 地区 | |
+| relationship_status | string | 感情状态 | |
+| sexual_orientation | string | 性取向 | |
+| self-intro | string | 简介 | |
+| tags | array | 用户标签 | 每个元素为标签字符串 |
+
+# 使用示例
+
+ ```python
+ # -*- coding: utf-8 -*-
+from wcrawler import *
+
+if __name__ == '__main__':
+    crawler = WCrawler(cookie = '_T_WM=1764ed14d4a61ef43ab86ce292307697; SUHB=0G0veCMff5FczB; SUB=_2A257Qs6KDeTxGeRP6VYZ9yrLzz2IHXVYzNLCrDV6PUJbrdAKLWvFkW1D2q2amgN3BCri-O-SXueGVG-2Rg..; gsid_CTandWM=4urb5d951ez5pXptyT36L8UMabX', \
+        max_num_weibo = 10, \
+        max_num_fans = 10, \
+        max_num_follow = 10, \
+        wfilter = ALL)
+    print crawler.crawl(url = 'http://weibo.cn/yaochen')
+ ```
+
+ # 示例输出结果
+
+    {
+    "birthday": "",
+    "fans": [
+        {
+            "nickname": "用户5760291495",
+            "num_fans": 2,
+            "url": "http://weibo.cn/u/5760291495",
+            "verify_info": ""
+        },
+        {
+            "nickname": "何多多1946057893",
+            "num_fans": 5,
+            "url": "http://weibo.cn/u/1946057893",
+            "verify_info": ""
+        },
+        {
+            "nickname": "用户5758816679",
+            "num_fans": 1,
+            "url": "http://weibo.cn/u/5758816679",
+            "verify_info": ""
+        },
+        {
+            "nickname": "汤昕瑜",
+            "num_fans": 7,
+            "url": "http://weibo.cn/u/5594790094",
+            "verify_info": ""
+        },
+        {
+            "nickname": "有你就好NZT",
+            "num_fans": 4,
+            "url": "http://weibo.cn/u/5663756456",
+            "verify_info": ""
+        },
+        {
+            "nickname": "加贝凡心",
+            "num_fans": 14,
+            "url": "http://weibo.cn/u/5672655434",
+            "verify_info": ""
+        },
+        {
+            "nickname": "疯子琴08",
+            "num_fans": 15,
+            "url": "http://weibo.cn/u/5370899326",
+            "verify_info": ""
+        },
+        {
+            "nickname": "夏白橙795",
+            "num_fans": 30,
+            "url": "http://weibo.cn/u/2406626575",
+            "verify_info": ""
+        },
+        {
+            "nickname": "小雨的快乐生活52",
+            "num_fans": 2,
+            "url": "http://weibo.cn/u/5756714081",
+            "verify_info": ""
+        },
+        {
+            "nickname": "用户5672942017",
+            "num_fans": 1,
+            "url": "http://weibo.cn/u/5672942017",
+            "verify_info": ""
+        }
+    ],
+    "follow": [
+        {
+            "fans": 798863,
+            "nickname": "毕飞宇",
+            "num_fans": -1,
+            "url": "http://weibo.cn/bifeiyu"
+        },
+        {
+            "fans": 33531,
+            "nickname": "编剧王小平",
+            "num_fans": -1,
+            "url": "http://weibo.cn/u/1150691890"
+        },
+        {
+            "fans": 8127049,
+            "nickname": "英国报姐",
+            "num_fans": -1,
+            "url": "http://weibo.cn/uktimes"
+        },
+        {
+            "fans": 177906,
+            "nickname": "早稻-野獸",
+            "num_fans": -1,
+            "url": "http://weibo.cn/u/1835883650"
+        },
+        {
+            "fans": 52687,
+            "nickname": "韩浩月",
+            "num_fans": -1,
+            "url": "http://weibo.cn/hanhaoyue"
+        },
+        {
+            "fans": 1713638,
+            "nickname": "故宫博物院",
+            "num_fans": -1,
+            "url": "http://weibo.cn/gugongweb"
+        },
+        {
+            "fans": 12054823,
+            "nickname": "Mr_凡先生",
+            "num_fans": -1,
+            "url": "http://weibo.cn/u/3591355593"
+        },
+        {
+            "fans": 27326,
+            "nickname": "洛梅笙",
+            "num_fans": -1,
+            "url": "http://weibo.cn/u/2034280670"
+        },
+        {
+            "fans": 2779,
+            "nickname": "企鹅奸妃",
+            "num_fans": -1,
+            "url": "http://weibo.cn/u/1165842462"
+        },
+        {
+            "fans": 2342918,
+            "nickname": "张译",
+            "num_fans": -1,
+            "url": "http://weibo.cn/yanyuanzhangyi"
+        }
+    ],
+    "gender": "女",
+    "location": "北京 朝阳区",
+    "nickname": "姚晨",
+    "num_fans": 78473998,
+    "num_follow": 456,
+    "num_weibo": 8240,
+    "relationship_status": "",
+    "self-intro": "",
+    "sexual_orientation": "",
+    "tags": [],
+    "url": "http://weibo.cn/yaochen",
+    "verify_info": "演员，联合国难民署中国亲善大使。",
+    "verify_type": "RED_V",
+    "weibo": [
+        "转发了 中国新闻网 的微博:【浙江丽水山体滑坡已致4死33失踪】昨晚22点50分左右，浙江丽水莲都区雅溪镇里东村发生山体滑坡，据当地新闻发布会上最新消息：①滑坡已致4人死亡，33人失踪，27户房屋被埋，21户房屋进水；②当地已转移群众300多人；③抢险救灾正全力展开，灾害原因正在进一步调查中。 [组图共9张]转发理由:愿逝者安息，早日找到生还者。[蜡烛][祈祷]  ",
+        "转发了 南方都市报 的微博:#巴黎遇袭#中国驻法国使馆建议，在法中国公民如无特殊需要，近期内尽量减少出行，密切跟踪法国当地媒体的相关报道和警方的安全提示，并配合警方的安全检查和证件检查。如遇紧急情况，可以拨打以下电话。@与欧洲有关的一切转发理由:转发微博  ",
+        "转发了 央视新闻 的微博:#巴黎恐怖袭击#【[话筒]紧急扩散：出国旅游一定记住电话12308】法国发生恐怖袭击事件，中国驻法国使馆发紧急通知，提醒在法中国公民注意安全。中国驻法使馆领事保护与协助电话：0033-615742537。外交部全球领保与服务应急呼叫中心电话：00861012308。不管同胞身处哪国，拨通电话，就能得到帮助！转！转发理由:[蜡烛] //@李晨:[祈祷]  ",
+        "转发了 头条新闻 的微博:【再次提醒：所有在法中国人留在家中，不要出门！】11月13日晚，法国遇恐怖袭击153人死亡。中国驻法使馆紧急通知，所有在法中国公民留在家中，避免出门。遇紧急情况请即拨报警电话17或197。中国使馆领保电话：0033-615742537；外交部应急呼叫电话：00861012308。新浪新闻正播报：http://t.cn/RUY2svA [组图共3张]转发理由:[伤心]  ",
+        "转发了 北京人不知道的北京事儿 的微博:#北京提个醒儿# 【五年不换手机号的人可信度高！】@法制晚报 ：据统计5年以上不换手机号码者，是个相当值得信赖的可交之友。据中、美、英、韩、俄等国抽样调查得出结论：若一个人的手机号码能8年以上保持不变，则可以判定这是一个非常值得信赖的人。你手机号有几年没换了？[嘻嘻] 是这样么[思考] [组图共2张]转发理由:没记错的话，我的手机号应该用了十四年了…[doge] //@亲爱的黄金:十年了我的已经 //@loverwg:我十年了 还欠着你的钱。。。。。。。。 //@加娟:我的六年了，从不欠别人钱  ",
+        "转发了 南方都市报 的微博:#午间分享#我们怀念的童年，一去不复返了。@复古老照片 [组图共9张]转发理由:童年的小溪，池塘和大树，都不知何时离开了我们…[白云]  ",
+        "真正有用的东西，其实是陪伴。🌛 http://t.cn/RUWyzme ",
+        "转发了 姚晨 的微博:爸妈不在家，老婆还没有，独自过双11。[doge]转发理由:这是爸妈的床，角度问题显大。土豆每天睡前都坐在这里看巧虎，等着妈妈回家，妈妈的怀抱就是他的温床。[张嘴][月亮] //@新浪娱乐:回复@Adela曾晓敏:小土豆只是想要一张小床[笑cry] //@Adela曾晓敏:每天在两百万平方米的床上醒来[眼泪]  ",
+        "爸妈不在家，老婆还没有，独自过双11。[doge] 原图 ",
+        "转发了 毕飞宇 的微博:【毕飞宇：关于《红楼梦》答新京报记者柏琳问】我一直建议年轻人去读《红楼梦》，尤其是独生子女，不读，就不会懂得中国的宗亲，就无法了解那个巨大的、隐形的、神秘的中国，就无法懂得中国的社会为什么会是“人情社会”，就没法认识“人情社会”的温暖和“人情社会”的邪恶。http://t.cn/RUNpfM2转发理由:转发微博  "
+        ]
+    }
+
+# 待实现的功能
+
+1.用email和密码组合来模拟登录
+
+登录过程的http重定向比较多，还在尝试中。。。
+
+# FAQ
+
+1.爬取大量数据会不会被新浪封禁账户？
+
+会的。我只碰到临时封禁的情况，过段时间自动解禁，建议注册马甲。
+
+2.不要爬取所登录账户的微博数据！！网页结构不一样，会挂掉的。
---
+++ wcrawler.py
@@ -0,0 +1,303 @@
+# -*- coding: utf-8 -*-
+import requests
+import json, re
+import os
+import urllib, urllib2, cookielib
+from bs4 import BeautifulSoup
+from copy import deepcopy
+
+RED_V, BLUE_V, VIP, ACTIVE = 'RED_V', 'BLUE_V', 'VIP', 'ACTIVE'
+verify_table = {'http://u1.sinaimg.cn/upload/2011/07/28/5338.gif': RED_V,
+                'http://u1.sinaimg.cn/upload/2011/07/28/5337.gif': BLUE_V,
+                'http://u1.sinaimg.cn/upload/h5/img/hyzs/donate_btn_s.png': VIP,
+                'http://u1.sinaimg.cn/upload/2011/08/16/5547.gif': ACTIVE}
+headers = { \
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', \
+        'Accept-Encoding':'gzip, deflate, sdch', \
+        'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6,pl;q=0.4,zh-TW;q=0.2,ru;q=0.2', \
+        'Connection':'keep-alive', \
+        'Cookie':'', \
+        'Host':'weibo.cn', \
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.132 Safari/537.36' \
+    }
+ALL, ORIGINAL = 0, 1
+class WCrawler:
+    def __init__(self,  cookie = None, \
+                        max_num_weibo = 10, \
+                        max_num_fans = 10, \
+                        max_num_follow = 10, \
+                        wfilter = ORIGINAL):
+        self.headers = headers
+        self.login_email = None
+        self.password = None
+        if cookie != None and len(cookie) > 0:
+            self.headers['Cookie'] = cookie
+        else:
+            self.headers['Cookie'] = self.__get_cookie()
+        self.max_num_weibo = max_num_weibo
+        self.max_num_fans = max_num_fans
+        self.max_num_follow = max_num_follow
+        self.wfilter = wfilter
+        self.data = None
+
+    def crawl(self, url = 'http://weibo.cn/yaochen'):
+        self.data = {'url': '', \
+                    'nickname': '', \
+                    'verify_type': '', \
+                    'verify_info': '', \
+                    'num_weibo': -1, \
+                    'num_fans': -1, \
+                    'num_follow': -1, \
+                    'gender': '', \
+                    'birthday': '', \
+                    'weibo': [], \
+                    'fans': [], \
+                    'follow': [], \
+                    'location': '', \
+                    'relationship_status': '', \
+                    'sexual_orientation': '', \
+                    'self-intro': '', \
+                    'tags': []
+                    }
+        self.data['url'] = self.__normalize_url(url)
+
+        # step 1: crawl weibo text
+        turl = self.data['url']
+        if self.wfilter == ORIGINAL: turl += '?filter=1'
+        req = self.__get_request(turl)
+        soup = BeautifulSoup(req.text, 'lxml')
+        n_page = self.__parse_max_pages(soup)
+        self.data['weibo'] = self.__parse_weibo(soup)
+        self.data['verify_type'] = self.__parse_user_verify_type(soup)
+        info_url = self.__parse_info_url(soup)
+        fans_url, follow_url = self.__parse_fans_and_follow_url(soup)
+        # page by page
+        for i in xrange(2, 1 + n_page):
+            if len(self.data['weibo']) >= self.max_num_weibo:
+                break
+            turl = self.data['url'] + '?page=' + str(i)
+            if self.wfilter == ORIGINAL: turl = self.data['url'] + '?filter=1&page=' + str(i)
+            req = self.__get_request(turl)
+            soup = BeautifulSoup(req.text, 'lxml')
+            self.data['weibo'] += self.__parse_weibo(soup)
+        self.data['weibo'] = self.data['weibo'][:self.max_num_weibo]
+
+        # step 2: crawl user's personal information
+        req = self.__get_request(info_url)
+        soup = BeautifulSoup(req.text, 'lxml')
+        self.__parse_info_list(soup)
+
+        # step 3: crawl user's fans list
+        req = self.__get_request(fans_url)
+        soup = BeautifulSoup(req.text, 'lxml')
+        fans_max_page = self.__parse_max_pages(soup)
+        self.data['fans'] += self.__parse_fans(soup)
+        for i in xrange(2, 1 + fans_max_page):
+            if len(self.data['fans']) >= self.max_num_fans:
+                break
+            req = self.__get_request(fans_url + '?page=' + str(i))
+            soup = BeautifulSoup(req.text, 'lxml')
+            self.data['fans'] += self.__parse_fans(soup)
+        self.data['fans'] = self.data['fans'][:self.max_num_fans]
+
+        # step 4: crawl user's follow list
+        req = self.__get_request(follow_url)
+        soup = BeautifulSoup(req.text, 'lxml')
+        follow_max_page = self.__parse_max_pages(soup)
+        self.data['follow'] += self.__parse_follow(soup)
+        for i in xrange(2, 1 + follow_max_page):
+            if len(self.data['follow']) > self.max_num_follow:
+                break
+            req = self.__get_request(follow_url + '?page=' + str(i))
+            soup = BeautifulSoup(req.text, 'lxml')
+            self.data['follow'] += self.__parse_follow(soup)
+        self.data['follow'] = self.data['follow'][:self.max_num_follow]
+
+        # step 5: return final result
+        return json.dumps(self.data, ensure_ascii = False, sort_keys = True, indent = 4).encode('utf-8', 'replace')
+
+    def __parse_follow(self, soup):
+        ret = []
+        for table in soup.find_all('table'):
+            cur = {'url': '', 'nickname': '', 'num_fans': -1}
+            cur['url'] = table.find_all('a')[0]['href']
+            cur['nickname'] = table.find_all('a')[1].get_text()
+            for e in table.find_all('td'):
+                for ch in e.children:
+                    try:
+                        if ch.startswith(u'粉丝'):
+                            cur['fans'] = int(ch.strip(u'粉丝').strip(u'人'))
+                    except:
+                        continue
+            ret.append(cur)
+        return ret
+
+    def __parse_info_list(self, soup):
+        arr = soup.find_all('div')
+        table = None
+        for i in xrange(len(arr)):
+            if arr[i]['class'][0] == 'tip' and arr[i].get_text() == u'基本信息':
+                assert(i + 1 < len(arr))
+                table = arr[i + 1]
+        assert(table != None)
+        for c in table.children:
+            try:
+                if c['href'].find('keyword') > 0:
+                    self.data['tags'].append(c.get_text())
+            except:
+                pass # do nothing...
+            pos = c.find(':')
+            if pos < 0 or pos + 1 == len(c):
+                continue
+            key, val = c[:pos], c[pos + 1:]
+            assert(len(key) > 0 and len(val) > 0)
+            if key == u'昵称': self.data['nickname'] = val
+            elif key == u'性别': self.data['gender'] = val
+            elif key == u'地区': self.data['location'] = val
+            elif key == u'生日': self.data['birthday'] = val
+            elif key == u'简介': self.data['self-intro'] = val
+            elif key == u'性取向': self.data['sexual_orientation'] = val
+            elif key == u'认证': self.data['verify_info'] = val
+            elif key == u'感情状况': self.data['relationship_status'] = val
+            else: print 'Not include attribute:', key, val
+
+    def __parse_fans_and_follow_url(self, soup):
+        table = soup.find_all('div', attrs = {'class': 'tip2'})[0]
+        fans_url, follow_url = None, None
+        for e in table.children:
+            try:
+                text = e.get_text()
+                if text.startswith(u'微博'):
+                    self.data['num_weibo'] = int(text.strip(u'微博|[|]'))
+                elif text.startswith(u'关注'):
+                    self.data['num_follow'] = int(text.strip(u'关注|[|]'))
+                    follow_url = self.__remove_qmark('http://weibo.cn' + e['href'])
+                elif text.startswith(u'粉丝'):
+                    self.data['num_fans'] = int(text.strip(u'粉丝|[|]'))
+                    fans_url = self.__remove_qmark('http://weibo.cn' + e['href'])
+            except:
+                continue
+        assert(follow_url != None and fans_url != None)
+        return fans_url, follow_url
+
+    def __parse_info_url(self, soup):
+        table = soup.find_all('table')[0]
+        for e in table.find_all('a'):
+            if e.get_text() == u'资料':
+                assert(e['href'][0] == '/')
+                ret = 'http://weibo.cn' + e['href']
+                return self.__remove_qmark(ret)
+        raise 'Error: can not find info tag.'
+
+    # def __get_cookie(self):
+    #     ck = cookielib.CookieJar()
+    #     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(ck))
+    #     urllib2.install_opener(opener)
+    #     page = opener.open("http://3g.sina.com.cn/prog/wapsite/sso/login.php")
+    #     data = page.read()
+    #     password = re.findall('<postfield name="([\S]*)" value="\$\(password\)" />',data)[0]
+    #     vk = re.findall('<postfield name="vk" value="([\S]*)" />',data)[0]
+    #     params = urllib.urlencode({"mobile": self.login_email, \
+    #                                 password: self.password, \
+    #                                 "vk": vk, \
+    #                                 "remember": "on", \
+    #                                 "backURL": "http://weibo.cn", \
+    #                                 "submit": "1"})
+    #     header = {"Content-Type": "application/x-www-form-urlencoded",
+    #                 "Referer": "http://3g.sina.com.cn/prog/wapsite/sso/login.php",
+    #                 "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0"
+    #             }
+    #     req = urllib2.Request("http://3g.sina.com.cn/prog/wapsite/sso/login.php", params, header)
+    #     page = urllib2.urlopen(req)
+    #     print page.read()
+    #     ret = ""
+    #     for item in ck:
+    #         ret += (item.name + '=' + item.value + ';')
+    #     return ret
+
+    def __get_request(self, url, max_try = 3):
+        cnt = 0
+        while cnt < max_try:
+            try:
+                req = requests.get(url, headers = self.headers)
+            except:
+                cnt += 1
+                continue
+            if req.status_code != requests.codes.ok:
+                break
+            return req
+        # Should not reach here if everything is ok.
+        print json.dumps(self.data, ensure_ascii = False, sort_keys = True, indent = 4).encode('utf-8', 'replace')
+        print 'Error:', url
+        assert(False)
+
+    def __parse_weibo(self, soup):
+        # return all weibo in a page as a list
+        ret = []
+        for block in soup.find_all('div', 'c'):
+            divs = block.find_all('div')
+            if len(divs) == 1:
+                text = self.__trim_like(divs[0].get_text())
+                ret.append(text)
+            elif len(divs) == 2 or len(divs) == 3:
+                text = self.__trim_like(divs[0].get_text())
+                comment = self.__trim_like(divs[-1].get_text())
+                ret.append(text + comment)
+            elif len(divs) == 0:
+                continue
+            else:
+                assert(False)
+        return ret
+
+    def __parse_max_pages(self, soup):
+        # return total number of pages for one's weibo
+        try:
+            res = int(soup.find(id='pagelist').get_text().split('/')[-1].strip(u'页'))
+        except:
+            return 1
+        return res
+
+    def __parse_user_verify_type(self, soup):
+        r = soup.find_all('div', attrs = {'class': 'ut'})[0].find_all('img', attrs = {'alt': 'V'})
+        if len(r) == 0:
+            return ''
+        src = r[0]['src']
+        if src in verify_table:
+            return verify_table[src]
+        return ''
+
+    def __parse_fans(self, soup):
+        res = []
+        for follow in soup.find_all('table'):
+            cur = {'url': '', 'verify_info': '', 'num_fans': -1, 'nickname': ''}
+            person = follow.find_all('td')[1]
+            cur['url'] = person.find('a')['href']
+            img = person.find_all('img')
+            if len(img) > 0:
+                src = img[0]['src']
+                if src in verify_table:
+                    cur['verify_info'] = verify_table[src]
+            pos = person.get_text().rfind(u'粉丝')
+            plain = person.get_text()
+            cur['nickname'] = plain[0:pos]
+            cur['num_fans'] = int(plain[pos + len(u'粉丝'):].split(u'人')[0])
+            res.append(cur)
+        return res
+
+    def __trim_like(self, s):
+        pos = s.rfind(u'赞')
+        if pos >= 0:
+            return s[:pos]
+        else:
+            return s
+
+    def __remove_qmark(self, url):
+        while url.find('?') >= 0:
+            url = url[:url.find('?')]
+        return url
+
+    def __normalize_url(self, url):
+        url = self.__remove_qmark(url)
+        url = url.strip('/')
+        url = url.replace('weibo.com', 'weibo.cn')
+        return url
